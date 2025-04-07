e9bfe02309bc719a9dd1e61ab5982a997ef3642d
achat <a.chatziangelou@student.tudelft.nl>
2025-04-07 14:19:34 +0200

    Updaded Wind Turbine set

--- 6. Approach_2_ninja_wt.py
+++ 6. Approach_2_ninja_wt.py
@@ -5,32 +5,32 @@ import re
 # New wind turbine dataset (updated with new models)
 turbines = [
     # IEC Class 1 turbines
-    {"Model": "swt 3 101", "Capacity": 3.0, "RotorDiameter": 101, "IEC_Class_Num": 1},
-    {"Model": "swt 4.3 120", "Capacity": 4.3, "RotorDiameter": 120, "IEC_Class_Num": 1},
-    {"Model": "SWT 8 154", "Capacity": 8.0, "RotorDiameter": 154, "IEC_Class_Num": 1},
-    {"Model": "swt 3.6 107", "Capacity": 3.6, "RotorDiameter": 107, "IEC_Class_Num": 1},
-    {"Model": "Siemens Gamesa SG 6 154", "Capacity": 6.0, "RotorDiameter": 154, "IEC_Class_Num": 1},
-    {"Model": "Siemens Gamesa SG 8,5 167", "Capacity": 8.5, "RotorDiameter": 167, "IEC_Class_Num": 1},
-    {"Model": "nordex 100 3300", "Capacity": 3.3, "RotorDiameter": 100, "IEC_Class_Num": 1},
+    {"Model": "Siemens SWT 3-101", "Capacity": 3.0, "RotorDiameter": 101, "IEC_Class_Num": 1},
+    {"Model": "Siemens SWT 4.3-120", "Capacity": 4.3, "RotorDiameter": 120, "IEC_Class_Num": 1},
+    {"Model": "Siemens SWT 8-154", "Capacity": 8.0, "RotorDiameter": 154, "IEC_Class_Num": 1},
+    {"Model": "Siemens SWT 3.6-107", "Capacity": 3.6, "RotorDiameter": 107, "IEC_Class_Num": 1},
+    {"Model": "Siemens Gamesa SG 6-154", "Capacity": 6.0, "RotorDiameter": 154, "IEC_Class_Num": 1},
+    {"Model": "Siemens Gamesa SG 8.5-167", "Capacity": 8.5, "RotorDiameter": 167, "IEC_Class_Num": 1},
+    {"Model": "Nordex 100-3300", "Capacity": 3.3, "RotorDiameter": 100, "IEC_Class_Num": 1},
 
     # IEC Class 2 turbines
-    {"Model": "e82 3000", "Capacity": 3.0, "RotorDiameter": 82, "IEC_Class_Num": 2},
-    {"Model": "V90 3 MW", "Capacity": 3.0, "RotorDiameter": 90, "IEC_Class_Num": 2},
-    {"Model": "v136 4 MW", "Capacity": 4.0, "RotorDiameter": 136, "IEC_Class_Num": 2},
-    {"Model": "Siemens Gamesa SG  4.5 145", "Capacity": 4.5, "RotorDiameter": 145, "IEC_Class_Num": 2},
+    {"Model": "E82 3000", "Capacity": 3.0, "RotorDiameter": 82, "IEC_Class_Num": 2},
+    {"Model": "Vestas V90-3.0", "Capacity": 3.0, "RotorDiameter": 90, "IEC_Class_Num": 2},
+    {"Model": "Vestas V136-4.0", "Capacity": 4.0, "RotorDiameter": 136, "IEC_Class_Num": 2},
+    {"Model": "Siemens Gamesa SG 4.5-145", "Capacity": 4.5, "RotorDiameter": 145, "IEC_Class_Num": 2},
 
     # IEC Class 3 turbines
-    {"Model": "Enercon E-115 3.000", "Capacity": 3.0, "RotorDiameter": 115, "IEC_Class_Num": 3},
-    {"Model": "SWT 6.6 170", "Capacity": 6.6, "RotorDiameter": 170, "IEC_Class_Num": 3},
-    {"Model": "Nordex 131 4000", "Capacity": 4.0, "RotorDiameter": 131, "IEC_Class_Num": 3},
-    {"Model": "Nordex N131 3000", "Capacity": 3.0, "RotorDiameter": 131, "IEC_Class_Num": 3},
+    {"Model": "Enercon E-115-3.000", "Capacity": 3.0, "RotorDiameter": 115, "IEC_Class_Num": 3},
+    {"Model": "Siemens SWT 6.6-170", "Capacity": 6.6, "RotorDiameter": 170, "IEC_Class_Num": 3},
+    {"Model": "Nordex 131-4000", "Capacity": 4.0, "RotorDiameter": 131, "IEC_Class_Num": 3},
+    {"Model": "Nordex N131-3000", "Capacity": 3.0, "RotorDiameter": 131, "IEC_Class_Num": 3},
 
     # IEC Class S turbines
-    {"Model": "e126 4000", "Capacity": 4.0, "RotorDiameter": 126, "IEC_Class_Num": 0},
-    {"Model": "e175 6000", "Capacity": 5.0, "RotorDiameter": 175, "IEC_Class_Num": 0},
-    {"Model": "v150 6 MW", "Capacity": 6.0, "RotorDiameter": 150, "IEC_Class_Num": 0},
-    {"Model": "v164 9500", "Capacity": 9.5, "RotorDiameter": 164, "IEC_Class_Num": 0},
-    {"Model": "Nordex 149 4500", "Capacity": 4.5, "RotorDiameter": 149, "IEC_Class_Num": 0}
+    {"Model": "Enercon E126-4000", "Capacity": 4.0, "RotorDiameter": 126, "IEC_Class_Num": 0},
+    {"Model": "Enercon E175-6000", "Capacity": 5.0, "RotorDiameter": 175, "IEC_Class_Num": 0},
+    {"Model": "Vestas V150-6.0", "Capacity": 6.0, "RotorDiameter": 150, "IEC_Class_Num": 0},
+    {"Model": "Vestas V164-9500", "Capacity": 9.5, "RotorDiameter": 164, "IEC_Class_Num": 0},
+    {"Model": "Nordex 149-4500", "Capacity": 4.5, "RotorDiameter": 149, "IEC_Class_Num": 0}
 ]
 
 def land_area(diameter, terrain_type):
